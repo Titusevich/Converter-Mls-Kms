bfe8b8b44e33bbf9866d3c0148ac399672ff7b6b
Titusevich <nickitatitaevskiy@gmail.com>
2025-02-02 23:37:21 +0200

    Add kilometres to miles convert

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,11 +12,19 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter amount of distance in miles: ");
         double mls = scan.nextDouble();
+        System.out.print("Enter amount of distance in kilometres: ");
+        double kms = scan.nextDouble();
         double kilometres = convertMilesToKilometres(mls);
-        System.out.printf("%nResult is %.2f kilometres.", kilometres);
+        double miles = convertKilometresToMiles(kms);
+        System.out.printf("%nResult is %.2f kilometres and %.2f miles.",
+                kilometres, miles);
     }
 
     private static double convertMilesToKilometres(double mls) {
         return mls * CONVERT_KM;
+    }
+
+    private static double convertKilometresToMiles(double kms) {
+        return kms / CONVERT_KM;
     }
 }
